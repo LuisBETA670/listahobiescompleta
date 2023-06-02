b2f1e7c55c64941da411035629c1c5b2c3c8c85d
luis Eduardo Betanzos IBañez <sc19350285@gmail.com>
2023-06-02 14:16:03 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name = "Jesus Lerdo Atilano";
+  return (
+    <div>
+    <h3> Lista de hobbies de {name}</h3>
+    <ul>
+    <li>Tocar guitarra</li>
+    <li>Jugar videojuegos</li>
+    <li>Cocinar</li>
+    </ul>
+    </div>
+  )
+}
+
+export default App
